b28ae11c9ef68a1ece15b9e8c6f10f8b8b655787
kimmingyu <alsrb2918@gmail.com>
2021-08-19 16:38:04 +0900

    Algorithm 문제풀이

---
+++ BOJ/basic1-4(1874).py
@@ -0,0 +1,28 @@
+# 1874번 스택수열 문제
+# 처음엔 이해 안갔지만, 하나씩 확인 해본 후 이해
+
+n = int(input())
+count = 0 # 오름차순
+stack = [] # 숫자가 들어갈 스택 (1,2,3,4,5,6,7,8,9 순서로)
+result = [] # 부호가 들어갈 스택
+no_message=True
+
+for i in range(0,n):
+    x = int(input())
+
+    while count < x:
+      count += 1
+      stack.append(count)
+      result.append("+")
+
+    if stack[-1]==x:
+        stack.pop()
+        result.append("-")
+    else:
+        no_message = False # 스택 구성이 안되면 False 로 변경 뒤 NO 출력
+        break
+
+if no_message==False:
+    print("NO")
+else:
+    print("\n".join(result))
