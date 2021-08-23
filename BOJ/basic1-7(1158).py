d6c5c6730ad12eb1609d648bc513318ed1b34d9f
kimmingyu <alsrb2918@gmail.com>
2021-08-23 12:55:46 +0900

    알고리즘 문제풀이 - 요세푸스 문제(1158번)

---
+++ BOJ/basic1-7(1158).py
@@ -0,0 +1,43 @@
+# 백준 1158번 요세푸스 문제 
+
+# 다른 사람 풀이 1
+
+N,K = map(int,input().split())
+arr = [i for i in range(1,N+1)]    # 맨 처음에 원에 앉아있는 사람들
+
+answer = []   # 제거된 사람들을 넣을 배열
+num = 0  # 제거될 사람의 인덱스 번호
+
+for t in range(N):
+    num += K-1  
+    if num >= len(arr):   # 한바퀴를 돌고 그다음으로 돌아올때를 대비해 값을 나머지로 바꿈  
+        num = num%len(arr)
+
+    answer.append(str(arr.pop(num)))
+
+print("<",", ".join(answer)[:],">", sep='')
+
+
+
+
+
+# 다른 사람 풀이 2
+
+N, K = map(int, input().split()) 
+circular_list = [] 
+answer = [] 
+
+for i in range(N): 
+  circular_list.append(i+1) 
+
+popNum = 0 
+  
+while len(circular_list) >0: 
+    popNum = (popNum + (K-1)) % len(circular_list) 
+
+    popElemnet = circular_list.pop(popNum) 
+    
+    answer.append(str(popElemnet)) 
+
+print("<%s>" %(", ".join(answer)))
+
